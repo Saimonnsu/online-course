9bfbd807b7a1c8e184839b4d8c1320ccbf9f0f78
Saimonnsu <saimonk1400@gmail.com>
2020-08-28 03:38:13 -0700

    finished task

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css'
+const Footer = () => {
+    return (
+        <div className="footer">
+            &copy; DN S Saimon | 2020
+        </div>
+    );
+};
+
+export default Footer;
